22a6a45ef5f3b597a427bffa536f0c4862e966df
zzgGit <32737891+zzgGit@users.noreply.github.com>
2017-10-29 10:26:02 +0800

    update Test
    update Test

--- src/com/zzg/Test.java
+++ src/com/zzg/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello,Git服务器");
+		System.out.println("hello,Git服务器使用人员");
 	}
 
 }
